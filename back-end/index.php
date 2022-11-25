51349140e40e6259349992a9fbe29b130854fe17
Harun-Sulaiman <114136224+Harun-Sulaiman@users.noreply.github.com>
2022-11-25 16:54:34 +0800

    Add files via upload

---
+++ back-end/index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Foor order Website</title>
+
+        <link rel="stylesheet" href="../css/back-end.css">
+    </head>
+
+    <body>
+        <div class=Menu>
+            <div class="wrapper">
+            hello
+            </div>
+        </div>
+
+        <div class=Content>
+            
+        </div>
+
+        <div class=footer>
+            
+        </div>
+
+    </body>
+</html>
