46c3c5ef105687990e2203a7b95afdb117d4d118
Harun-Sulaiman <114136224+Harun-Sulaiman@users.noreply.github.com>
2022-11-28 21:21:03 +0800

    Update

--- back-end/index.php
+++ back-end/index.php
@@ -6,18 +6,55 @@
     </head>
 
     <body>
-        <div class=Menu>
+        <div class="menu text-center">
             <div class="wrapper">
-            hello
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Admin</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Food</a></li>
+                    <li><a href="#">Order</a></li>
+                </ul>
             </div>
         </div>
 
         <div class=Content>
-            
+            <div class="wrapper">
+                <h1>DASHBOARD</h1>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br/>
+                    Categories
+                </div>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br/>
+                    Categories
+                </div>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br/>
+                    Categories
+                </div>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br/>
+                    Categories
+                </div>
+
+                <div class="clearfix"></div>
+
+            </div>
         </div>
 
         <div class=footer>
-            
+            <div class="wrapper">
+            <p class="text-center">IMS607 Food Order & Management System</p>
+            </div>
         </div>
 
     </body>
