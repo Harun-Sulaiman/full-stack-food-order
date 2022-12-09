76682d38a9589549e42f182a72467d0ac5bb5248
Azrun_SS <114136224+Azrun-00@users.noreply.github.com>
2022-12-09 18:05:30 +0800

    Add login page

---
+++ back-end/login.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="../css/admin-page.css">
+    </head>
+
+    <body>
+        <div class="login">
+            <h1>Login</h1>
+
+
+            <p>IMS607 Food Order & Management System</p>
+
+        </div>
+
+
+
+    </body>
+</html>
--- back-end/partials/menu.php
+++ back-end/partials/menu.php
@@ -2,7 +2,7 @@
 
 <html>
     <head>
-        <title>Foor order & Management</title>
+        <title>Foor order & Management System</title>
 
         <link rel="stylesheet" href="../css/admin-page.css">
     </head>
