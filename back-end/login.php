70abf7263ec539328e4e5470c5473bee968495f0
Harun-Sulaiman <114136224+Harun-Sulaiman@users.noreply.github.com>
2022-12-10 22:52:48 +0800

    Update login page

--- back-end/login.php
+++ back-end/login.php
@@ -1,3 +1,5 @@
+<?php include ('../config/constants.php'); ?>
+
 <html>
     <head>
         <title>Login</title>
@@ -6,10 +8,32 @@
 
     <body>
         <div class="login">
-            <h1>Login</h1>
+            <h1 class="text-center">Log In</h1>
+            <br/>
+            <form action="" method="POST">
+                    
+                <div class="div-login">
+                    <input type="text" name="user_name" placeholder="Enter Username">
+                    <br/> <br/>
+                    
+                    <input type="password" name="password" placeholder="Enter Password">
+                    <br/> <br/>
+                        
+                    <input type="submit" name="submit" value="Enter" class="btn-login2">
+                </div>
+            </form>
 
+            <br/><br/>
+            <?php
+                if(isset($_SESSION['login']))
+                {
+                    echo $_SESSION['login'];
+                    unset ($_SESSION['login']);
+                }
+            ?>
 
-            <p>IMS607 Food Order & Management System</p>
+            <br/> <br/>
+            <p class="text-center">IMS607 Food Order & Management System</p>
 
         </div>
 
@@ -17,3 +41,36 @@
 
     </body>
 </html>
+
+<?php
+    //check enter 
+    if(isset($_POST['submit']))
+    {
+        //get data
+        $user_name = $_POST['user_name'];
+        $password = md5($_POST['password']);
+
+        //sql query check
+        $sql = "SELECT * FROM admin WHERE user_name='$user_name' AND password='$password'";
+
+        //execute query
+        $res = mysqli_query($conn, $sql);
+
+        //count database to check the user
+        $count = mysqli_num_rows($res);
+
+            if($count==1)
+            {
+                //user correct
+                $_SESSION ['login'] = "<div class='success'>Login Success.</div>";
+                header('location:'.HOMEPAGE.'back-end/');
+            }
+            else
+            {
+                //no user
+                $_SESSION ['login'] = "<div class='error text-center'>Login Failed.</div>";
+                header('location:'.HOMEPAGE.'back-end/login.php');
+            }
+    }
+
+?>
