6973eed1a520ab26db4cd3c38818e8acd614daf0
Harun-Sulaiman <114136224+Harun-Sulaiman@users.noreply.github.com>
2022-12-28 20:58:50 +0800

    Live server constant

--- config/constants.php
+++ config/constants.php
@@ -3,17 +3,20 @@
     session_start();
 
 
-        //store no repeat
-    define('HOMEPAGE','http://localhost/ims607-food-order/full-stack-food-order/');
+    //store no repeat
+    //phpmyadmin localhost xamp
+    //define('HOMEPAGE','http://localhost/ims607-food-order/full-stack-food-order/');
     //define('LOCALHOST','localhost');
     //define('DB_USERNAME','root');
     //define('DB_PASSWORD','');
     //define('DB_NAME','ims607');
 
+    //live server
+    define('HOMEPAGE','https://ricomtr.com/8ag5/full-stack-food-order/');
     define('LOCALHOST','localhost');
-    define('DB_USERNAME','ricomtrc_8ag5'); //online
-    define('DB_PASSWORD','8ag5#vHy@2938'); //online
-    define('DB_NAME','ricomtrc_8ag5'); //online
+    define('DB_USERNAME','ricomtrc_8ag5'); 
+    define('DB_PASSWORD','8ag5#vHy@2938'); 
+    define('DB_NAME','ricomtrc_8ag5'); 
 
         //for live database use this code, $conn = mysqli_connect('localhost','username','passsword') or die(mysqli_error());
     $conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die(mysqli_error()); //connect database
